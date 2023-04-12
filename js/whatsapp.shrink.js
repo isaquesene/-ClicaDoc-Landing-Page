8613dbff951f298a234cfa71de5d01d04e8700e7
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-12 17:52:46 -0300

    comit

--- js/whatsapp.shrink.js
+++ js/whatsapp.shrink.js
@@ -1,9 +1,14 @@
 const content = document.querySelector('[data-shrink="yes"]');
 
 
-const span = document.querySelector('[data-shrink="yes"] span');
+const span = document.querySelector('[data-shrink="yes"] span')
 
 
 setInterval(function(){
     content.classList.toggle('shrink')
 },5000)
+
+
+setInterval(function(){
+    span.classList.toggle('shrink')
+}, 5000)
